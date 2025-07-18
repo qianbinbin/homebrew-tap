e030db81b8325b6ee4f76d799150ef21d645ec74
Binbin Qian <qianbinbin@hotmail.com>
2025-07-19 00:33:22 +0800

    Create rclone formula

---
+++ Formula/rclone.rb
@@ -0,0 +1,37 @@
+class Rclone < Formula
+  desc "Rsync for cloud storage"
+  homepage "https://rclone.org/"
+  license "MIT"
+  version "1.70.3"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-osx-amd64.zip"
+      sha256 "1616b25d5c5fd07a52498d09480a4fdbeb42e0d21cd3246d2d7df5dddd5ce35a"
+    end
+
+    on_arm do
+      url "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-osx-arm64.zip"
+      sha256 "14a9c5eb9f699a749470c898974412092eee204d74d3395486e3307c255021f7"
+    end
+  end
+
+  conflicts_with "rclone", because: "both install `rclone` binaries"
+
+  def install
+    bin.install "rclone"
+    man1.install "rclone.1"
+    system bin/"rclone", "genautocomplete", "bash", "rclone.bash"
+    system bin/"rclone", "genautocomplete", "zsh", "_rclone"
+    system bin/"rclone", "genautocomplete", "fish", "rclone.fish"
+    bash_completion.install "rclone.bash" => "rclone"
+    zsh_completion.install "_rclone"
+    fish_completion.install "rclone.fish"
+  end
+
+  test do
+    (testpath/"file1.txt").write "Test!"
+    system bin/"rclone", "copy", testpath/"file1.txt", testpath/"dist"
+    assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
+  end
+end
