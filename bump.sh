4bb9ab1c637e694cb10c5fceb0b6b4b2ec84cd37
Binbin Qian <qianbinbin@hotmail.com>
2025-07-26 02:22:45 +0800

    Add --force option for bump.sh

--- bump.sh
+++ bump.sh
@@ -2,12 +2,35 @@
 
 set -euo pipefail
 
+FORCE=false
+
 error() { printf '%s\n' "$@" >&2; }
 
+USAGE=$(
+  cat <<-END
+Usage: $0 [OPTION]... [FILE]
+Bump a single formula/cask file; exit with 0 only when the file has been
+updated.
+
+Options:
+  -f, --force         try to update even the version is already the latest;
+                      useful when the developer re-released the same version
+  -h, --help          display this help and exit
+
+Home page: <https://github.com/qianbinbin/homebrew-tap>
+END
+)
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+  -f | --force) FORCE=true && shift ;;
+  -h | --help) error "$USAGE" && exit ;;
+  *) break ;;
+  esac
+done
+
 if [ $# -ne 1 ] || [ ! -f "$1" ] || [[ "$1" != *.rb ]]; then
-  error "Usage: $0 FILE" \
-    "Update a single formula/cask file; exit with 0 only when the file has been updated."
-  exit 2
+  error "$USAGE" && exit 2
 fi
 
 if sed --version 2>/dev/null | grep -qs GNU; then
@@ -38,17 +61,20 @@ update_gh_release() {
   new_ver=${new_tag#v}
   new_ver=${new_ver#jq-} # fixup for jq
 
+  ver_changed=false
   if [ "$old_ver" = "$new_ver" ]; then
-    error "$pkg_name is already the newest version ($old_ver)."
-    return 1
+    error "$pkg_name is already the latest version ($old_ver)."
+    $FORCE || return 1
+  else
+    error "  $old_ver -> $new_ver"
+    sed_i "s/version \"$old_ver\"$/version \"$new_ver\"/" "$_file"
+    ver_changed=true
   fi
 
-  error "$old_ver -> $new_ver"
-  sed_i "s/version \"$old_ver\"$/version \"$new_ver\"/" "$_file"
-
+  sha256_changed=false
   for url in $urls; do
     old_sha256=$(grep -A 1 "$url" "$_file" | sed -En 's/^ *sha256 "([^"]+)"$/\1/p')
-    [ -n "$old_sha256" ] || continue
+    [ -n "$old_sha256" ] || { error "sha256 not configured for $url" && continue; }
     new_name=$(basename "$url" | sed "s/#{version}/$new_ver/")
     new_sha256=$(
       jq -r ".assets[] | select(.name == \"$new_name\") | .digest" "$TMP_DIR/$pkg_name.json" |
@@ -56,14 +82,20 @@ update_gh_release() {
     )
     if [ -z "$new_sha256" ]; then
       new_url="${url//\#\{version\}/$new_ver}"
-      error "$pkg_name: sha256 not found on GitHub, downloading to compute..." "$new_url"
+      error "sha256 not found on GitHub, downloading to compute..." "$new_url"
       curl -L --retry 4 "$new_url" -o "$TMP_DIR/$new_name"
       new_sha256=$(sha256sum "$TMP_DIR/$new_name" | awk '{ print $1 }')
     fi
-    error "$old_sha256 -> $new_sha256"
-    sed_i "s/$old_sha256/$new_sha256/" "$_file"
+    if [ "$old_sha256" = "$new_sha256" ]; then
+      error "sha256 did't change for $url"
+    else
+      error "  $old_sha256 -> $new_sha256"
+      sed_i "s/$old_sha256/$new_sha256/" "$_file"
+      sha256_changed=true
+    fi
   done
-  error "Updated $pkg_name."
+  $ver_changed || $sha256_changed || return 1
+  error "Updated $_file."
 }
 
 url=$(sed -En 's|^ *url "(.+)"$|\1|p' "$1" | head -1)
