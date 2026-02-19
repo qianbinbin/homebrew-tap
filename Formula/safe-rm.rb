d0a20856416803fc8c716a4978862aeeea83108f
Binbin Qian <qianbinbin@hotmail.com>
2026-02-19 22:32:13 +0800

    Update safe-rm formula

--- Formula/safe-rm.rb
+++ Formula/safe-rm.rb
@@ -4,7 +4,16 @@ class SafeRm < Formula
   license "MIT"
   head "https://github.com/kaelzhang/shell-safe-rm.git", branch: "master"
 
+  conflicts_with "safe-rm", because: "both install `safe-rm` binaries"
+
   def install
-    bin.install "bin/rm.sh" => "rm"
+    bin.install "bin/rm.sh" => "safe-rm"
+  end
+
+  def caveats
+    <<~EOS
+      To use it as "rm", add the following to your shell profile e.g. ~/.profile or ~/.zshrc:
+        alias rm='safe-rm'
+    EOS
   end
 end
