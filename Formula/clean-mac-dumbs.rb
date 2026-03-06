143c8603388868d742535090043ad7d87a92dddd
Binbin Qian <qianbinbin@hotmail.com>
2026-03-07 00:21:22 +0800

    Update clean-mac-dumbs URL

--- Formula/clean-mac-dumbs.rb
+++ Formula/clean-mac-dumbs.rb
@@ -4,7 +4,7 @@ class CleanMacDumbs < Formula
   license "MIT"
   version "1.0"
 
-  url "https://qianbinbin.github.io/code/cleaning-dumb-dotfiles-on-macos/clean-mac-dumbs-#{version}.tar.gz"
+  url "https://qianbinbin.github.io/posts/cleaning-dumb-dotfiles-on-macos/clean-mac-dumbs-#{version}.tar.gz"
   sha256 "6da2c0a1add6a058581b061cdead3041b08a018a5154b84242b8e5afac90a42f"
 
   depends_on :macos
